e25f9df93a3abb950d6b06dfcf439a755f6d0251
Ivan <devdnia@gmail.com>
2022-06-10 14:44:31 +0200

    logout

--- src/components/journal/Sidebar.js
+++ src/components/journal/Sidebar.js
@@ -1,6 +1,15 @@
+import { useDispatch } from "react-redux"
+import { startLogout } from "../../actions/auth"
 import { JournalEntries } from "./JournalEntries"
 
 export const Sidebar = () => {
+
+    const dispatch = useDispatch()
+
+    const handleLogout = () => {
+        dispatch( startLogout() );
+    }
+
     return (
         <aside className="journal__sidebar">
 
@@ -10,7 +19,10 @@ export const Sidebar = () => {
                     <span> Iván</span>
                 </h3>
 
-                <button className="btn">
+                <button 
+                    className="btn"
+                    onClick={ handleLogout }
+                >
                     Logout
                 </button>
             </div>
