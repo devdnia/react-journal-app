6aa7b074cd360edf1856d1f84531449e84b3fa5e
Ivan <devdnia@gmail.com>
2022-06-10 12:35:56 +0200

    loading general

---
+++ src/components/loading/LoadingScreen.js
@@ -0,0 +1,15 @@
+
+export const LoadingScreen = () => {
+    return (
+
+        <div className="loading__container">
+            <div className="loading__title animate__animated animate__fadeIn">
+                Loading
+                <p className="loading__title animate__animated animate__fadeIn animate__faster animate__infinite	infinite">.</p>
+                <p className="loading__title animate__animated animate__fadeIn animate__fast animate__infinite	infinite">.</p>
+                <p className="loading__title animate__animated animate__fadeIn animate animate__infinite	infinite">.</p>
+            </div>
+        </div>
+
+    )
+}
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { firebase } from '../firebase/firebase-config';
 import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom';
@@ -6,10 +6,18 @@ import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom';
 import { AuthRouter } from './AuthRouter';
 import { JournalScreen } from '../components/journal/JournaScreen';
 import { login } from '../actions/auth';
+import { LoadingScreen } from '../components/loading/LoadingScreen';
 
 export const AppRouter = () => {
 
     const dispatch = useDispatch();
+
+    // Para condicionar rutas, si esta autenticado,
+    // mientras firebase revisa la autentificación
+    const [ checking, setChecking ] = useState(true);
+
+    // Para redireccionar al usuario en las rutas
+    const [ isLoggedIn, setLoggedIn ] = useState(false);
 
     // Cuando cambia la autenticación de Firebase
     useEffect(() => {
@@ -19,19 +27,31 @@ export const AppRouter = () => {
             // Si el obj user existe pregunta el uid
             if( user?.uid){
                 dispatch( login( user.uid, user.displayName ) );
+                setLoggedIn( true );
+            } else {
+                setLoggedIn( false );
             }
+
+            setChecking( false );
         })
 
-    }, [ dispatch ]);
+    }, [ dispatch, setChecking, setLoggedIn ]);
     
-    console.log('Prueba github')
+    if( checking ){
+        return(
+            <LoadingScreen />
+        )
+    }
+
     return (
         <>
             <BrowserRouter>
                 <Routes>
-
+                    
+                    {/* Si el usuario NO está autenticado */}
                     <Route path="/auth/*" element={<AuthRouter />} />
 
+                    {/* Si el usuario esta autenticado */}
                     <Route path="/" element={<JournalScreen />} />
 
                     <Route path="*" element={<Navigate replace to="/auth/login" />} />
